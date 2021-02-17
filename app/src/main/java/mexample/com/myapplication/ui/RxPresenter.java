ca2519c158c3a34f404ffcbaf067230e8a280d1e
Zoltan Majlath <majlath.tech@gmail.com>
2021-02-17 22:28:56 +0100

    Simplify RX thread sending, encapsulate thread scheduling, use generic functions parameters to be flexible

--- app/src/main/java/mexample/com/myapplication/ui/RxPresenter.java
+++ app/src/main/java/mexample/com/myapplication/ui/RxPresenter.java
@@ -1,7 +1,11 @@
 package mexample.com.myapplication.ui;
 
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class RxPresenter<S>  extends Presenter<S> {
     private CompositeDisposable compositeSubscription;
@@ -27,8 +31,25 @@ public class RxPresenter<S>  extends Presenter<S> {
         super.detachScreen();
     }
 
-
-    public void attachSubscription(Disposable subscription) {
+    public void attachDisposable(Disposable subscription) {
         compositeSubscription.add(subscription);
+    }
+
+    private <T> Observable<T> scheduleThreads(Observable<T> o) {
+        return o.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    protected <T> void performRequest(Observable<T> o, Consumer<T> onSuccess, Consumer<? super Throwable> onError) {
+        attachDisposable(scheduleThreads(o).subscribe(onSuccess, onError));
+    }
+
+    protected <T> void performRequest(Observable<T> o, Consumer<T> s) {
+        attachDisposable(scheduleThreads(o).subscribe(s, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }));
     }
 }
--- app/src/main/java/mexample/com/myapplication/ui/main/MainPresenter.java
+++ app/src/main/java/mexample/com/myapplication/ui/main/MainPresenter.java
@@ -4,9 +4,7 @@ import android.content.Context;
 
 import javax.inject.Inject;
 
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
 import mexample.com.myapplication.R;
 import mexample.com.myapplication.network.interactor.WeatherInteractor;
 import mexample.com.myapplication.network.model.CurrentWeather;
@@ -24,20 +22,17 @@ public class MainPresenter extends RxPresenter<MainScreen> {
     }
 
     public void getWeather(String city) {
-        attachSubscription(weatherInteractor.getWeather(city)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<CurrentWeather>() {
-                    @Override
-                    public void accept(CurrentWeather currentWeather) {
-                        screen.showWeather(currentWeather);
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) {
-                        throwable.printStackTrace();
-                        screen.showError(context.getString(R.string.network_error));
-                    }
-                }));
+        performRequest(weatherInteractor.getWeather(city), new Consumer<CurrentWeather>() {
+            @Override
+            public void accept(CurrentWeather currentWeather) {
+                screen.showWeather(currentWeather);
+            }
+        }, new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) {
+                throwable.printStackTrace();
+                screen.showError(context.getString(R.string.network_error));
+            }
+        });
     }
 }
